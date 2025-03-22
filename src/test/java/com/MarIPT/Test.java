a170c45baafc3c3b9938d1db85ad2e3c1275d032
Induja93 <ninduja.93@gmail.com>
2025-03-22 13:03:48 +0530

    induja method created

--- src/test/java/com/MarIPT/Test.java
+++ src/test/java/com/MarIPT/Test.java
@@ -1,9 +1,14 @@
 package com.MarIPT;
 
-public class Test {
-	
-	public void nvn() {
-		System.out.println();
-	}
+import com.MarIPT.base.BaseClass;
 
+public class Test extends BaseClass{
+	
+	public static void main(String[] args) {
+		browserLaunch("chrome");
+		launchUrl("https://www.google.com/");
+		System.out.println("BROWSER id:" + Thread.currentThread().getId());
+		exitBrowser(driver);
+	}
+	
 }
